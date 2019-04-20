707d75caf83b4cc5ac3aa39d74815876829479d1
DaoPD <daopd@fsoft.com.vn>
2019-04-20 15:18:40 +0700

    [HFW] update

--- upload/catalog/controller/extension/hfwmodule/hfwservice.php
+++ upload/catalog/controller/extension/hfwmodule/hfwservice.php
@@ -8,7 +8,7 @@ class ControllerExtensionHfwmoduleHfwservice extends Controller {
 		
 		$this->load->model('extension/shipping/hfwsample');
 		$service_id = $this->request->get['service_id'];
-		$this->session->data['ups_service_id'] =  $service_id;
+		$this->session->data['hfw_service_id'] =  $service_id;
 		//Set Price
 		$total = $this->model_extension_shipping_hfwsample->getPriceService($service_id);
 		echo $total;
@@ -18,12 +18,6 @@ class ControllerExtensionHfwmoduleHfwservice extends Controller {
 		$select = $this->language->get('text_select'); 
 		$open = $this->language->get('text_open'); 
 		$close = $this->language->get('text_close'); 
-		
-		
-		$full_address = $this->request->post['full_address'];
-		$country_code = $this->request->post['country_code'];
-		$maximum_size = $this->request->post['maximum_size'];
-		$nearby = $this->request->post['nearby'];
 		$html = "<p>Locator</p>";
 		$this->response->setOutput($html);
 	}
